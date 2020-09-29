1365a26f2ef6ee19dc1afeff10b85607724267c6
velann21 <velan@reynencourt.com>
2020-09-29 08:46:53 +0200

    Added the events hook

---
+++ pkg/routes/users.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/velann21/match_todo_gateway_srv/pkg/controller"
+	"github.com/velann21/match_todo_gateway_srv/pkg/service"
+	pf "github.com/velann21/todo-commonlib/proto_files/users_srv"
+)
+
+type UsersRoutes struct {
+	UsersServiceGrpcClient pf.UserManagementServiceClient
+}
+
+func (i *UsersRoutes) ApiRoutes(route *mux.Router){
+	gwc := controller.UserServiceController{
+		Service:service.New(i.UsersServiceGrpcClient),
+	}
+	route.PathPrefix("/users/registration").HandlerFunc(gwc.UsersRegistrationController).Methods("POST")
+	route.PathPrefix("/users/permission").HandlerFunc(gwc.CreatePermissionsController).Methods("POST")
+	route.PathPrefix("/users/roles").HandlerFunc(gwc.CreateRolesController).Methods("POST")
+	route.PathPrefix("/users/migration").HandlerFunc(gwc.SqlMigrationController).Methods("POST")
+}
